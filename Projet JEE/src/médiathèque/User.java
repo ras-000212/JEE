955e4e7a6a0641c7da2d5cc04fc002b1d49da564
Alexis SOK <sok@iut.univ-paris5.fr>
2020-03-05 10:20:55 +0100

    adding médiathèque

---
+++ Projet JEE/src/médiathèque/User.java
@@ -0,0 +1,21 @@
+
+public class User {
+	private String login;
+	private String password;
+	
+
+	
+	public User(String login, String password) {
+		this.login = login;
+		this.password = password;
+	}
+
+
+	public String toString() {
+		return this.login;
+	}
+	
+	
+
+
+}
